2cf8997a7c6bb25a71fd1d66c6093b55f14a821b
Lorenzo Vivarelli <lorenzo.vivarelli@studenti.unimi.it>
2024-11-11 17:00:57 +0100

    creazione variabile useState

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState  } from 'react';
 
 export default function App() {
+const [delivertext, setDelivertext] = useState('loading...');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{delivertext}</Text>
       <StatusBar style="auto" />
     </View>
   );
