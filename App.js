f195094f01644822c9f9edb4bb3cec1eed9b436a
Lorenzo Vivarelli <lorenzo.vivarelli@studenti.unimi.it>
2024-11-11 17:06:34 +0100

    aggiunta del primo use effect

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useState  } from 'react';
+import React, { useEffect, useState  } from 'react';
 
 export default function App() {
 const [delivertext, setDelivertext] = useState('loading...');
+
+useEffect(() => {
+  console.log('component loaded for the first time ');
+}, []); // la dipendenza vuota [] assicura che il codice venga eseguito solo una volta
+
 
 
   return (
